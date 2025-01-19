5712fd43a7a793338b77d0eae7f913b565c0ed10
kendrickdave <asogbaabiodun92@gamil.com>
2025-01-19 17:54:52 +0100

    project continuation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,11 +7,13 @@ import SignUp from './Pages/SignUp'
 import Dashboard from './Pages/Dashboard'
 
 import Projects from './Pages/Projects'
+import Header from './component/Header'
 
 
 export default function App() {
   return (
     <BrowserRouter>
+     <Header />
      <Routes>
      <Route path="/" element={<Home />}/>
      <Route path="/about" element={<About />}/>

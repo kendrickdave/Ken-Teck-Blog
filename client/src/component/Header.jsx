4c972516e11084001c29d2ad24d05b53a6772715
Asogba Abiodun kendrick <108404889+kendrickdave@users.noreply.github.com>
2025-02-15 10:58:00 +0000

    work in progress

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -1,16 +1,17 @@
 import { Button, Navbar, TextInput } from 'flowbite-react';
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link ,useLocation} from 'react-router-dom';
 import {AiOutlineSearch} from "react-icons/ai";
 import {FaMoon} from  'react-icons/fa';
 
 export default function Header() {
+  const path = useLocation().pathname
   return (
     <Navbar className='border-b-2'>
       <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
       <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500
        to-pink-500 rounded-full text-white'>
-          K-T</span> 
+          Mebo</span> 
        Blog
       </Link>
       {/* search input for desktop view  */}
@@ -29,12 +30,43 @@ export default function Header() {
       </Button>
       
       {/* light and dark mode control */}
-      <div className=''>
-       <Button>
+      <div className="flex gap-2 md:order-2">
+       <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
         <FaMoon/>
        </Button>
-      </div>
 
+       {/* sign in button */}
+       <Link to="/SignIn">
+       <Button gradientDuoTone='purpleToBlue'> 
+        Sign In
+         </Button>
+       </Link>
+       {/* hangbuger button */}
+       <Navbar.Toggle/>
+      </div>
+      
+
+
+       {/* Menu */}
+       <Navbar.Collapse>
+        <Navbar.Link active={path === '/Home'} as={'div'}>
+          <Link to ="/Home">
+          Home
+          </Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/About'} as={'div'}>
+          <Link to ="/About">
+          About Us
+          </Link>
+        </Navbar.Link>
+
+        {/* i use project.jsx for job page */}
+        <Navbar.Link active={path === '/Projects'} as={'div'}>
+          <Link to ="/Projects">
+          Jobs
+          </Link>
+        </Navbar.Link>
+       </Navbar.Collapse>
     </Navbar>
   );
 }

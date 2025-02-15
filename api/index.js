32620b24d55f8f93495ba42725094a9d54f8fff0
Asogba Abiodun kendrick <108404889+kendrickdave@users.noreply.github.com>
2025-02-15 15:02:28 +0000

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 
 // databse connection
 dotenv.config();
@@ -20,3 +21,7 @@ app.listen(3000, () => {
     "server is running on port 3000 my first server running on express"
   );
 });
+
+
+//api creation
+app.use('/api/user', userRouter);
